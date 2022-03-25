ef3848e64aea5a9796ec03c7b5831613fef59f5c
yuuuzzzin <uiuiui118437@gmail.com>
2022-03-26 01:04:47 +0900

    Feat: Paging을 위한 PagingSource 클래스 구현

---
+++ app/src/main/java/com/example/networkpractice/repository/datasource/CultureByRealmDataSource.kt
@@ -0,0 +1,29 @@
+package com.example.networkpractice.repository.datasource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.networkpractice.network.api.CultureService
+import com.example.networkpractice.network.model.CultureModel
+import javax.inject.Inject
+
+class CultureByRealmDataSource
+@Inject
+constructor(private val service: CultureService
+): PagingSource<Int, CultureModel>() {
+
+    override fun getRefreshKey(state: PagingState<Int, CultureModel>): Int {
+        return 0
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CultureModel> {
+        return try {
+            val page = params.key?: 1
+            val results = service.getCultureListByRealm(page = page).body!!.cultureList.toMutableList()
+            val nextPage = if(results.count() == 10) page + 1 else null
+            LoadResult.Page(data = results, nextKey = nextPage, prevKey = null)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            LoadResult.Error(e)
+        }
+    }
+}
