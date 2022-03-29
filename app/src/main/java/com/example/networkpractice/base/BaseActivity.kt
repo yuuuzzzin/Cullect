b297f3fdf9e84d1031c92f47cada6b81f9ef4337
yuuuzzzin <uiuiui118437@gmail.com>
2022-03-30 01:51:30 +0900

    Chore: Status Bar 투명하게 변경

--- app/src/main/java/com/example/networkpractice/base/BaseActivity.kt
+++ app/src/main/java/com/example/networkpractice/base/BaseActivity.kt
@@ -1,9 +1,15 @@
-package com.yuuuzzzin.recordnize.base
+package com.example.networkpractice.base
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewbinding.ViewBinding
+import com.example.networkpractice.R
 
 abstract class BaseActivity<VB : ViewBinding>(
     private val inflater: (LayoutInflater) -> VB,
@@ -15,5 +21,17 @@ abstract class BaseActivity<VB : ViewBinding>(
         super.onCreate(savedInstanceState)
         binding = inflater(layoutInflater)
         setContentView(binding.root)
+        setStatusBar()
+    }
+
+    private fun setStatusBar() {
+        if (Build.VERSION.SDK_INT <=  Build.VERSION_CODES.LOLLIPOP_MR1) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = (binding.root.background as ColorDrawable).color
+        }
     }
 }
--- app/src/main/java/com/example/networkpractice/base/BaseBindingActivity.kt
+++ app/src/main/java/com/example/networkpractice/base/BaseBindingActivity.kt
@@ -1,10 +1,16 @@
-package com.yuuuzzzin.offoff_android.utils.base
+package com.example.networkpractice.base
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.example.networkpractice.R
 
 abstract class BaseBindingActivity<VB : ViewDataBinding>(
     @LayoutRes
@@ -16,6 +22,17 @@ abstract class BaseBindingActivity<VB : ViewDataBinding>(
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutRes)
-        //binding.lifecycleOwner = this
+        setStatusBar()
+    }
+
+    private fun setStatusBar() {
+        if (Build.VERSION.SDK_INT <=  Build.VERSION_CODES.LOLLIPOP_MR1) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = (binding.root.background as ColorDrawable).color
+        }
     }
 }
