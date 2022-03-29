2e18d62affbef3ace428a4c2971c972fab7d875e
yuuuzzzin <uiuiui118437@gmail.com>
2022-03-30 01:52:20 +0900

    Chore: Grid 형태의 리사이클러뷰 아이템 간 space 지정

---
+++ app/src/main/java/com/example/networkpractice/util/RecyclerViewUtil.kt
@@ -0,0 +1,57 @@
+package com.example.networkpractice.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+object RecyclerViewUtil {
+
+    // 리사이클러뷰 아이템 간의 간격 설정
+    class VerticalSpaceItemDecoration(private val verticalSpaceHeight: Int) :
+        RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(
+            outRect: Rect, view: View, parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            outRect.bottom = verticalSpaceHeight
+        }
+    }
+
+    // 그리드 리사이클러뷰 아이템 간의 간격 설정
+    class GridSpaceItemDecoration(
+        private val spanCount: Int,
+        private val spacing: Int,
+        private val includeEdge: Boolean
+    ) : RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State,
+        ) {
+            val position = parent.getChildAdapterPosition(view)
+            val column = position % spanCount
+            if (includeEdge) {
+                outRect.left =
+                    spacing - column * spacing / spanCount
+                outRect.right =
+                    (column + 1) * spacing / spanCount
+                if (position < spanCount) {
+                    outRect.top = spacing
+                }
+                outRect.bottom = spacing
+            } else {
+                outRect.left = column * spacing / spanCount
+                outRect.right =
+                    spacing - (column + 1) * spacing / spanCount
+                if (position >= spanCount) {
+                    outRect.top = spacing
+                }
+            }
+        }
+    }
+
+}
