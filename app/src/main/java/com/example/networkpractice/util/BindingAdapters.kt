e2e22b765dd9dfa1c9c0d35a732e7193da88ed4b
yuuuzzzin <uiuiui118437@gmail.com>
2022-03-30 01:53:36 +0900

    Feat: 이미지 로딩 중일 시 쉬머 효과 구현

--- app/src/main/java/com/example/networkpractice/util/BindingAdapters.kt
+++ app/src/main/java/com/example/networkpractice/util/BindingAdapters.kt
@@ -1,10 +1,10 @@
 package com.example.networkpractice.util
 
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.networkpractice.R
-import com.example.networkpractice.util.ResUtil.getDrawable
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerDrawable
 
@@ -13,10 +13,23 @@ object BindingAdapters {
     @JvmStatic
     @BindingAdapter("url")
     fun setImageViewUrl(view: AppCompatImageView, url: String?) {
+
+        val shimmer: Shimmer = Shimmer.ColorHighlightBuilder()
+            .setBaseAlpha(0.8f)
+            .setHighlightAlpha(0.9f)
+            .setDuration(1800)
+            .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+            .setAutoStart(true)
+            .build()
+
+        val shimmerDrawable = ShimmerDrawable().apply {
+            setShimmer(shimmer)
+        }
+
         url?.let {
             Glide.with(view.context).load(it)
                 .placeholder(shimmerDrawable)
-                .error(getDrawable(R.drawable.img_null))
+                .error(ContextCompat.getDrawable(view.context, R.drawable.img_null))
                 .into(view)
         }
     }
